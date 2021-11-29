9560002c61f472a5d0fdd087b226fd740d7de2a7
DeborahLuna <deborahdlmb@gmail.com>
2021-11-29 14:40:30 -0300

    Organização dos arquivos

---
+++ modulo1/semana1/aula-variaveis/index.js
@@ -0,0 +1,53 @@
+// Como fazer um comentário!
+
+// comenta uma linha
+
+/*este é um texto grande que 
+vai virar um bloco */
+
+//console.log("Olá Mundo")
+
+/* const nomeDoUsuario = prompt("Olá! Qual seu nome?")
+console.log(nomeDoUsuario) */
+
+/* const idade = 31
+console.log(idade)
+idade = 32 */
+
+
+/* let idade = 31
+idade = 32
+console.log(idade) */
+
+
+/* const nome = prompt("Qual seu nome?")
+const idade = prompt ("Qual sua idade")
+
+console.log("Olá, meu nome é", nome, "e tenho", idade, "anos")
+ */
+
+/* const nome = "Déborah"
+const sobrenome = "Luna"
+const idade = 31
+const ehEstudante = true
+console.log("meu nome é", nome, sobrenome, "e tenho", idade, "anos de idade. Ainda estudo?", ehEstudante) */
+
+
+/*const nome = "Déborah"
+const sobrenome = "Luna"
+const idade = 31
+const ehEstudante = true*/
+
+/* console.log("nome", typeof nome)
+console.log("sobrenome", typeof sobrenome)
+console.log("idade", typeof idade)
+console.log("status", typeof ehEstudante) */
+
+//TRANSFORMANDO OS TIPOS
+
+/*const nomeUsuario = prompt("qual seu nome")
+const idadeUsuario = prompt ("qual sua idade")
+const idadeUsuarioNumero = Number (idadeUsuario)
+
+const idadeUsuarioNumero = */
+
