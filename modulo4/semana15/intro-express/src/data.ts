b7a592d4310ce310ac03d03f6ba82f341e2efdb9
DeborahLuna <deborahdlmb@gmail.com>
2022-03-29 14:42:11 -0300

    Exercício Intro Express

---
+++ modulo4/semana15/intro-express/src/data.ts
@@ -0,0 +1,78 @@
+export const users = [
+    {
+        id: 1,
+        name: "Déborah",
+        phone: "99999-5555",
+        website: "www.deborahluna.com",
+        posts: [
+            {
+                id: 10,
+                title: "Blablabla",
+                body: "Bla bla bla",
+                userId: 1
+            },
+            {
+                id: 473467,
+                title: "Ble",
+                body: "iyubfnuwrpuohf",
+                userId: 1
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Hugo",
+        phone: "99559-7777",
+        website: "www.hugobraganca.com",
+        posts: [
+            {
+                id: 12654,
+                title: "ABCD@1234",
+                body: "usrnoarspc",
+                userId: 2
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Luiz Felipe",
+        phone: "99999-2233",
+        website: "www.luizfelipe.com",
+        posts: [
+            {
+                id: 36503987501,
+                title: "Eu amo a Olivia",
+                body: "Pimpinha á linda",
+                userId: 3
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "Olivia",
+        phone: "99999-0000",
+        website: "www.pimpinha.com",
+        posts: [
+            {
+                id: 214235,
+                title: "Quero petisco",
+                body: "Gosto de lamber patas",
+                userId: 4
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: "Thor",
+        phone: "99999-1010",
+        website: "www.thorcarente.com",
+        posts: [
+            {
+                id: 254334,
+                title: "Não sei fazer xixi no lugar certo",
+                body: "É muito difícil",
+                userId: 5
+            }
+        ]
+    }
+]
--- modulo4/semana15/intro-express/src/index.ts
+++ modulo4/semana15/intro-express/src/index.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { Express, Request, Response } from 'express';
 import { AddressInfo } from "net";
-import cors from "cors"
+import cors from "cors";
+import { users } from "./data"
 
 const app = express()
 
@@ -19,4 +20,64 @@ const server = app.listen(process.env.PORT || 3003, () => {
 //Exercício 01
 app.get("/", (req, res)=> {
     res.send("Hello from Express")
+    res.status(201).send("Primeiro endpoint criado!")
+})
+
+//Exercício 02
+type user = {
+    id: number,
+    name: string,
+    phone: string,
+    email: string,
+    website: string,
+    posts: any[]
+}
+
+//Exercício 03
+
+//arquivo data.ts criado
+
+//Exercício 04
+app.get("/users", (req, res)=> {
+    const allUsers = users
+    res.send(allUsers)
+    res.status(201).send("Usuários")
+})
+
+//Exercício 05
+type post = {
+    id: number,
+    title: string,
+    body: string,
+    userId: number
+}
+
+//Exercício 06
+//arrays de posts criados
+//melhor criar dentro do array de usuários, fica mais fácil de acessar pelos endpoints
+
+
+//Exercício 07
+
+app.get("/posts", (req, res)=> {
+    const allPosts = users.map((user) => {
+        return user.posts
+    })
+    const resultPosts = allPosts.flat(1)
+    res.send(resultPosts)
+    res.status(201).send("Posts")
+})
+
+//Exercício 08
+
+app.get("/posts/:userId", (req, res)=> {
+    const userId = Number(req.params.userId)
+    const allUsers = users
+    const allUserPosts = allUsers.map((user) => {
+        if(user.id === userId){
+            return user.posts
+        }
+    }).flat(1)
+    res.send(allUserPosts)
+    res.status(201).send("UserPosts")
 })
