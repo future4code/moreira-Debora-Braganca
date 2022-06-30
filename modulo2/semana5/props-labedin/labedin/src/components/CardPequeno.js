9e909226b9277bbff55ca210231a31665a091439
DeborahLuna <88689419+DeborahLuna@users.noreply.github.com>
2022-06-30 18:41:36 -0300

    Delete modulo2 directory

--- modulo2/semana5/props-labedin/labedin/src/components/CardPequeno.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './CardPequeno.css'
-
- export function CardPequeno(props) {
-     return <div className="littlecard-container">
-         <p><img src={props.imagem}/>E-mail: {props.email}</p>
-         <p><img src={props.imagem}/>Endereço: {props.endereco}</p>
-     </div>
- }
